2318122d2de7f00dcafa7254f874ee17cc1a5e63
sinazay123 <sinazay123@gmail.com>
2022-11-18 18:53:25 -0500

    added time to live

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,8 @@ export default {
   port: 1337,
   dbUri: "mongodb://localhost:27017/repeat",
   saltWorkFactor: 10,
+  accessTokenTtl: "15m",
+  refreshTokenTtl: "1y",
   publicKey: `-----BEGIN PUBLIC KEY-----
   MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXzZerpx9qdaelwt1U7NCpWXQK
   km1OW4ohDF/7g01xDtYf8Nox9wzhhVQrFD+G4eaJoWxIhJYQTgT4ijMlpjXs07Mc
