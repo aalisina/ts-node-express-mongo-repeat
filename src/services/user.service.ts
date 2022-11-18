424dc59849322fe6682693f4593642e5d68525d3
sinazay123 <sinazay123@gmail.com>
2022-11-17 20:38:16 -0500

    Also omitted the comparePassword method

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,9 @@ import { DocumentDefinition } from "mongoose";
 import UserModel, { UserDocument } from "../models/user.model";
 
 export async function createUser(
-  input: DocumentDefinition<Omit<UserDocument, "createdAt" | "updatedAt">>
+  input: DocumentDefinition<
+    Omit<UserDocument, "createdAt" | "updatedAt" | "comparePassword">
+  >
 ) {
   try {
     return await UserModel.create(input);
