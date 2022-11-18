063c8b8f0ffbe5f3c57401d7ae738f3a0df3f497
sinazay123 <sinazay123@gmail.com>
2022-11-17 20:04:56 -0500

    Added user controller and service

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { createUser } from "../services/user.service";
+import logger from "../utils/logger";
+
+export async function createUserHandler(req: Request, res: Response) {
+  try {
+    const user = await createUser(req.body);
+    res.status(201).send(user);
+  } catch (error: any) {
+    logger.error(error);
+    // 409 conflict, user with the email already registered
+    res.status(409).send(error.message);
+  }
+}
---
+++ src/services/user.service.ts
@@ -0,0 +1,10 @@
+import { DocumentDefinition } from "mongoose";
+import UserModel, { UserDocument } from "../models/user.model";
+
+export async function createUser(input: DocumentDefinition<UserDocument>) {
+  try {
+    return await UserModel.create(input);
+  } catch (err: any) {
+    throw new Error(err);
+  }
+}
