cedeec8dd4ae6dcfc2baf6c2c1b5ce5efbdb1471
sinazay123 <sinazay123@gmail.com>
2022-11-19 20:00:09 -0500

    Added a findUser service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { DocumentDefinition } from "mongoose";
+import { DocumentDefinition, FilterQuery } from "mongoose";
 import UserModel, { UserDocument } from "../models/user.model";
 import { omit } from "lodash";
 
@@ -29,4 +29,8 @@ export async function validatePassword({
   const isValid = await user.comparePassword(password);
   if (!isValid) return false;
   return omit(user.toJSON(), "password");
+}
+
+export async function findUser(query: FilterQuery<UserDocument>) {
+  return UserModel.findOne(query).lean();
 }
